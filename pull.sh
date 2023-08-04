01d5e3cf80048db4a7a9319ec4f4ae30ca6986f3
Syrone Wong <wong.syrone@gmail.com>
2023-08-04 13:55:28 +0800

    update to 2.12-3
    split for grub2 and grub2-sign build
    fix structure for debian packaging
    
    to get source: apt-get source grub-efi-amd64-signed
    to get official log: getbuildlog grub-efi-amd64-signed last
    
    we can only generate once otherwise the version is malformed
    like 1+1+2.99+9

--- pull.sh
+++ pull.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -ex
-VERSION=2.06
-REVISION=13
+. ./version.sh
 
 wget http://deb.debian.org/debian/pool/main/g/grub2/grub2_$VERSION-$REVISION.debian.tar.xz
 tar xf grub2_$VERSION-$REVISION.debian.tar.xz
@@ -14,13 +13,18 @@ rm debian/patches/efi-variable-storage-minimise-writes.patch
 sed -i.bak 's/efi-variable-storage-minimise-writes.patch//' debian/patches/series
 rm debian/patches/series.bak
 
+# follow removal of efi-variable-storage-minimise-writes.patch
+rm debian/patches/efivar-check-that-efivarfs-is-writeable.patch
+sed -i.bak 's/efivar-check-that-efivarfs-is-writeable.patch//' debian/patches/series
+rm debian/patches/series.bak
+
 cp zpool-degraded-vdev.patch debian/patches
 echo 'zpool-degraded-vdev.patch' >> debian/patches/series
 
 cp rectify-terminal-io.patch debian/patches
 echo 'rectify-terminal-io.patch' >> debian/patches/series
 
-echo -e "$(cat changelog)\n\n$(cat debian/changelog)" > debian/changelog
+echo -e "$(cat changelog)\n\n" > debian/changelog
 
 sed -i.bak "s/deb_version\s*:=.\+/deb_version\t\t:= "'"'"$VERSION-$REVISION"'"'"/" debian/rules
 rm debian/rules.bak
